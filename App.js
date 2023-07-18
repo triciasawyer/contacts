a67faca6963e5c203a28c30740d566f2399952bc
Tricia Sawyer <tricia.sawyer03@gmail>
2023-07-18 14:20:28 -0500

    add messaging functionality for fun

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, Linking, FlatList } from 'react-native';
 import * as Contacts from 'expo-contacts';
+import * as SMS from 'expo-sms'; 
 import { useEffect, useState } from 'react';
 
 
@@ -21,18 +22,29 @@ const call = (contact) => {
       })
       .catch(err => console.error(err));
   } else {
-    console.error("Contact does not have a valid phone number.");
+    console.error("This contact does not have a valid phone number.");
   }
 };
 
+const message = async (contact) => {
+  const phoneNumber = contact.phoneNumbers?.[0]?.number;
+  if (phoneNumber) {
+    const messageText = "Hey, wyd?";
+    try {
+      await SMS.sendSMSAsync([phoneNumber], messageText);
+      console.log("Message sent successfully!");
+    } catch (error) {
+      console.error("Error sending the message:", error);
+    }
+  } else {
+    console.error("This contact does not have a valid phone number.");
+  }
+};
 
 const Item = ({ item }) => (
   <View style={styles.item}>
-    <Button
-      onPress={() => call(item)}
-      title={item.name}
-      color="white"
-    />
+    <Button onPress={() => call(item)} title={item.name} color="white" />
+    <Button onPress={() => message(item)} title="Message" color="white" />
   </View>
 );
 
